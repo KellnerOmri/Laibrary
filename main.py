d0cae028edc91b16de02435b841e0e8dc4f6f0c9
KellnerOmri <74428538+KellnerOmri@users.noreply.github.com>
2022-01-16 21:15:59 +0200

    create ui

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    from tkinter import *
 
+    window = Tk()
+    # add widgets here
+    window.title('Welcome To The Best Library!')
+    window.geometry("700x700+500+500")
+    student_button = Button(window, text="sign as student", fg='blue')
+    student_button.place(x=100, y=250)
+    lecture_button = Button(window, text="sign as lecture", fg='red')
+    lecture_button.place(x=250, y=250)
+    window.mainloop()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
